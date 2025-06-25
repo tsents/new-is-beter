087f11da6ccc30d15e6b482db6dcbdc223b6c469
tsents <tsents.sagie@gmail.com>
2025-06-25 15:53:30 +0300

    Add pretty formatting of ethernet.

--- ethernet_protocol.py
+++ ethernet_protocol.py
@@ -15,6 +15,12 @@ def numeric_mac(mac : str) -> bytes:
     """
     return bytes(int(mac[i:i + 2], 16) for i in range(0, len(mac), 3))
 
+def pretty_mac(raw_mac : bytes) -> str:
+    """
+    Convert mac form raw bytes format, into ff:ff:ff:.. format.
+    """
+    return ":".join(hex(i)[2:] for i in raw_mac)
+
 def ethernet_protcol(raw_frame : bytes, my_mac : bytes, promisc : bool) -> Optional[Tuple[bytes, bytes, bytes]]:
     """
     Parses raw bytes (raw_frame) into the packet using ethernet protocol.
@@ -25,9 +31,14 @@ def ethernet_protcol(raw_frame : bytes, my_mac : bytes, promisc : bool) -> Optio
     @return:            The data if given. 
     """
     dst_mac, src_mac, ether_type = struct.unpack(ETH_PROTOCOL, raw_frame[:HEADER_SIZE])
-    print(dst_mac, type(dst_mac), my_mac)
     if not promisc:
         if not (dst_mac == my_mac or dst_mac == BROADCAST):
             return None
     unparsed_data = raw_frame[HEADER_SIZE:]
     return unparsed_data, src_mac, ether_type
+
+def craft_ethernet(src_mac : bytes, dst_mac : bytes, data : bytes, protocol_type : bytes) -> bytes:
+    """
+    Crafts an ethernet frame, based on all fields that exist in ethernet.
+    """
+    return dst_mac + src_mac + protocol_type + data
