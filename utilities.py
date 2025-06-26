53889970c0ad9acd9bcb7c4ab9d74df00d9d4349
tsents <tsents.sagie@gmail.com>
2025-06-26 19:02:21 +0300

    Better naming of util functions.

--- utilities.py
+++ utilities.py
@@ -1,24 +1,30 @@
 from scapy.all import IFACES
 
 def my_mac(interface : str) -> bytes:
-    return raw_mac(IFACES.get(interface).mac)
+    """
+    Return the mac address of the interface. in bytes format.
+    """
+    return bytes_mac(IFACES.get(interface).mac)
 
 def my_ip(interface : str) -> bytes:
-    return raw_ip(IFACES.get(interface).ip)
-
-def raw_ip(pretty_ip : str) -> bytes:
     """
-    Formats ip from *.*.*.* string to raw bytes format. 
+    return the ip address of the interface. in bytes format.
+    """
+    return bytes_ip(IFACES.get(interface).ip)
+
+def bytes_ip(pretty_ip : str) -> bytes:
+    """
+    Formats ip from *.*.*.* string to network bytes format. 
     """
     return bytes(int(i) for i in pretty_ip.split("."))
 
 def pretty_ip(raw_ip : bytes) -> str:
     """
-    Formats ip bytes from raw format, to *.*.*.* string
+    Formats ip bytes from network bytes format, to *.*.*.* string
     """
     return ".".join([str(i) for i in raw_ip])
 
-def raw_mac(mac : str) -> bytes:
+def bytes_mac(mac : str) -> bytes:
     """
     Convert mac from string format 0c:fa:78:54:32:12 to bytes.
     """
